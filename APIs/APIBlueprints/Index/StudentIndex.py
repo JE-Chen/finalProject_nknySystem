6d9f45e492d4a733867000b1ef1b858ff68cd1f4
MattWang877 <74702129+MattWang877@users.noreply.github.com>
2020-12-24 16:23:23 +0800

    change name
    StudentIndex

---
+++ APIs/APIBlueprints/Index/StudentIndex.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+from flask_cors import cross_origin
+
+StudentIndex = Blueprint('StudentIndex', __name__)
+
+
+@StudentIndex.route(r'/StudentIndex')
+@cross_origin()
+def student_index_page():
+    return render_template('/Grade/StudentIndex.html')
