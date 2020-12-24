8dc60667453b7033a4e4bdb44ed52d544007ed67
MattWang877 <74702129+MattWang877@users.noreply.github.com>
2020-12-24 16:20:53 +0800

    change name
    ManagerIndex ProfessorIndex

--- APIs/APIBlueprints/Index/ManagerIndex.py
+++ APIs/APIBlueprints/Index/ManagerIndex.py
@@ -4,7 +4,7 @@ from flask_cors import cross_origin
 ManagerIndex = Blueprint('ManagerIndex', __name__)
 
 
-@StudentGradeList.route(r'/ManagerIndex')
+@ManagerIndex.route(r'/ManagerIndex')
 @cross_origin()
 def manager_index_page():
     return render_template('/Grade/ManagerIndex.html')
