681807582a7ea2d0514ffcf2fd92d1787e776b5e
JE-Chen <zenmailman@gmail.com>
2020-12-29 07:38:46 +0800

    update config

--- Project/APIBlueprints/LoginPage/ForgotPassword.py
+++ Project/APIBlueprints/LoginPage/ForgotPassword.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, redirect
 from flask_cors import cross_origin
 
+from Project.Resource import RestfulAPIResource
+
 ForgotPassword = Blueprint('ForgotPassword', __name__)
+
+Gmail = RestfulAPIResource.Gmail
+verification_code = RestfulAPIResource.VerificationCode
 
 
 @ForgotPassword.route(r'/ForgotPassword')
 @cross_origin()
 def forgot_password_page():
     return render_template('/LoginPage/ForgotPassword.html')
+
+
+@ForgotPassword.route(r'/ForgotPassword/GET/Gmail', methods=['POST', ])
+@cross_origin()
+def forgot_password_gmail():
+    if request.method == 'POST':
+        verification_code.generate_base64_image(5, 40, True)
+    return redirect(url_for('ForgotPassword.forgot_password_page'))
